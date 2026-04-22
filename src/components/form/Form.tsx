6ba5d147e586e2e34ba35d22da5a1bb2b8745a3f
Galaceon <antoniogarcialtr@gmail.com>
2026-04-22 19:26:26 +0200

    Typing cities and countries in the state

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -1,7 +1,22 @@
 import { countries } from "../../data/countries";
 import styles from './Form.module.css'
+import { useState, type ChangeEvent } from "react";
+import type { SearchType } from "../../types";
 
 export default function Form() {
+
+    const [search, setSearch] = useState<SearchType>({
+        city: '',
+        country: ''
+    })
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>) => {
+        setSearch({
+            ...search,
+            [e.target.name] : e.target.value
+        })
+    }
+
     return (
         <form className={styles.form}>
 
@@ -12,12 +27,19 @@ export default function Form() {
                     type="text"
                     name="city"
                     placeholder="Ciudad"
+                    value={search.city}
+                    onChange={handleChange}
                 />
             </div>
 
             <div className={styles.field}>
-                <label htmlFor="city">País: </label>
-                <select>
+                <label htmlFor="country">País: </label>
+                <select
+                    id="country"
+                    value={search.country}
+                    name="country"
+                    onChange={handleChange}
+                >
                     <option value="">Selecciona un País</option>
                     {countries.map(country => (
                         <option
