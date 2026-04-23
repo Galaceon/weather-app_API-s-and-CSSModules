b709985a43a37acea5c6196dd49be5ca3b10712f
Galaceon <antoniogarcialtr@gmail.com>
2026-04-23 21:23:58 +0200

    using env vars

--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -5,7 +5,7 @@ export default function useWeather() {
 
     const fetchWeather = async (search: SearchType) => {
 
-        const appId = 'd6afaca78dd634b685cb77688078312c'
+        const appId = import.meta.env.VITE_API_KEY
 
         try {
             const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${appId}`
