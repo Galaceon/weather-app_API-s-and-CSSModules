b93375deb6842c9a1e1cdb91e206ec2f0b716327
Galaceon <antoniogarcialtr@gmail.com>
2026-04-23 21:14:43 +0200

    first API call

--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -1,9 +1,22 @@
-
+import axios from "axios"
+import type { SearchType } from "../types"
 
 export default function useWeather() {
 
-    const fetchWeather = () => {
-        console.log('consultando hehe....')
+    const fetchWeather = async (search: SearchType) => {
+
+        const appId = 'd6afaca78dd634b685cb77688078312c'
+
+        try {
+            const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${appId}`
+
+            const {data} = await axios(geoUrl)
+            
+            console.log(data)
+
+        } catch (error) {
+            console.log(error)
+        }
     }
 
     return {
