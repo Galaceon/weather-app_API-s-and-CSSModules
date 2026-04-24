6134d215f0d190c5706e9cde1f5318f481858fd2
Galaceon <antoniogarcialtr@gmail.com>
2026-04-24 23:35:26 +0200

    Typing API results with valibot

--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -1,19 +1,20 @@
 import axios from "axios"
-import { z } from 'zod'
 import type { SearchType } from "../types"
+import { object, string, number, InferOutput, parse } from 'valibot'
 
 
-// Zod
-const Weather = z.object({
-    name: z.string(),
-    main: z.object({
-        temp: z.number(),
-        temp_max: z.number(),
-        temp_min: z.number()
+// Valibot
+const WeatherSchema = object({
+    name: string(),
+    main: object({
+        temp: number(),
+        temp_max: number(),
+        temp_min: number()
     })
 })
 
-type Weather = z.infer<typeof Weather>
+
+type Weather = InferOutput<typeof WeatherSchema>
 
 export default function useWeather() {
 
@@ -30,13 +31,12 @@ export default function useWeather() {
 
             const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appId}`
 
-            // Zod
+            // Valibot
             const {data: weatherResult} = await axios(weatherUrl)
-            const result = Weather.safeParse(weatherResult)
+            const result = parse(WeatherSchema, weatherResult)
 
-            if(result.success) {
-                console.log(result.data.main.temp)
-                console.log(result.data.name)
+            if(result) {
+                console.log(result.name)
             }
 
         } catch (error) {
